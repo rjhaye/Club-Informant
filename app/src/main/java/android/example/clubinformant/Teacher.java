547ec6b39833a82e8be4529d065ea13cda0cb628
RJhaye Austria <rhonjhaye.austria0208@gmail.com>
2022-01-08 06:34:28 +0800

    Database Teacher

--- app/src/main/java/android/example/clubinformant/Teacher.java
+++ app/src/main/java/android/example/clubinformant/Teacher.java
@@ -4,6 +4,16 @@ public class Teacher {
     private String teacherFName;
     private String teacherLName;
     private String teacherEmail;
+    private String id;
+    private String status;
+
+    public Teacher(String teacherFName, String teacherLName, String teacherEmail, String id, String status) {
+        this.teacherFName = teacherFName;
+        this.teacherLName = teacherLName;
+        this.teacherEmail = teacherEmail;
+        this.id = id;
+        this.status = status;
+    }
 
     public String getTeacherFName() {
         return teacherFName;
@@ -27,5 +37,21 @@ public class Teacher {
 
     public void setTeacherEmail(String teacherEmail) {
         this.teacherEmail = teacherEmail;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
--- app/src/main/java/android/example/clubinformant/TeacherRegistrationForm.java
+++ app/src/main/java/android/example/clubinformant/TeacherRegistrationForm.java
@@ -4,8 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class TeacherRegistrationForm extends AppCompatActivity {
     private EditText fname;
@@ -13,6 +22,7 @@ public class TeacherRegistrationForm extends AppCompatActivity {
     private EditText eMail;
     private EditText password;
     private EditText registrationKey;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +37,37 @@ public class TeacherRegistrationForm extends AppCompatActivity {
         eMail = findViewById(R.id.et_teacher_email_sign_up);
         password = findViewById(R.id.et_teacher_password_sign_up);
         registrationKey = findViewById(R.id.et_teacher_registration_key_sign_up);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void signUpBtn(View view) {
-
+        if (fname.getText().toString().isEmpty() || lname.getText().toString().isEmpty() || eMail.getText().toString().isEmpty() || password.getText().toString().isEmpty() ||
+                registrationKey.getText().toString().isEmpty()) {
+            Toast.makeText(TeacherRegistrationForm.this, "Empty field detected.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(eMail.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(TeacherRegistrationForm.this, "Registered Successfully!", Toast.LENGTH_SHORT).show();
+                    saveInfo();
+                    Intent intent = new Intent(TeacherRegistrationForm.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     public void goToSignInBtn(View view) {
         Intent signInActivity = new Intent(TeacherRegistrationForm.this, SignInActivity.class);
         startActivity(signInActivity);
+    }
+
+    public void saveInfo() {
+        DatabaseReference userReference = FirebaseDatabase.getInstance().getReference("Users/" + FirebaseAuth.getInstance().getCurrentUser().getUid());
+        Teacher newUser = new Teacher(fname.getText().toString(), lname.getText().toString(), eMail.getText().toString(),
+                FirebaseAuth.getInstance().getCurrentUser().getUid(), "Teacher");
+        userReference.setValue(newUser);
     }
 }
