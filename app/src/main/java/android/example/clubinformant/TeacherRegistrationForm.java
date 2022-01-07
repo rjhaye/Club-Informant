24ea36d7b20da82ea8c0a2fe1cf2500a17eb41d6
RJhaye Austria <rhonjhaye.austria0208@gmail.com>
2022-01-08 04:16:37 +0800

    Setting up Firebase

--- app/src/main/java/android/example/clubinformant/TeacherRegistrationForm.java
+++ app/src/main/java/android/example/clubinformant/TeacherRegistrationForm.java
@@ -3,19 +3,30 @@ package android.example.clubinformant;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TeacherRegistrationForm extends AppCompatActivity {
+    private EditText fname;
+    private EditText lname;
+    private EditText eMail;
+    private EditText password;
+    private EditText registrationKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_registration_form);
+        initWidgets();
     }
 
     public void initWidgets() {
-
+        fname = findViewById(R.id.et_teacher_fname_sign_up);
+        lname = findViewById(R.id.et_teacher_lname_sign_up);
+        eMail = findViewById(R.id.et_teacher_email_sign_up);
+        password = findViewById(R.id.et_teacher_password_sign_up);
+        registrationKey = findViewById(R.id.et_teacher_registration_key_sign_up);
     }
 
     public void signUpBtn(View view) {
