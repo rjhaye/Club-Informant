46af9f78bf8ea1fdb5c5213be102355a6f67d5f6
RJhaye Austria <rhonjhaye.austria0208@gmail.com>
2022-01-08 02:54:12 +0800

    Added the teacher registration form

--- app/src/main/java/android/example/clubinformant/MainActivity.java
+++ app/src/main/java/android/example/clubinformant/MainActivity.java
@@ -1,13 +1,36 @@
 package android.example.clubinformant;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void initWidgets() {
+
+    }
+
+    public void teacherRegisterBtn(View view) {
+        Intent teacherRegistrationForm = new Intent(MainActivity.this, TeacherRegistrationForm.class);
+        startActivity(teacherRegistrationForm);
+    }
+
+    public void studentRegisterBtn(View view) {
+        Intent studentRegistrationForm = new Intent(MainActivity.this, StudentRegistrationForm.class);
+        startActivity(studentRegistrationForm);
+    }
+
+    public void signInBtn(View view) {
+        Intent signIn = new Intent(MainActivity.this, TeacherRegistrationForm.class);
+        startActivity(signIn);
     }
 }
---
+++ app/src/main/java/android/example/clubinformant/TeacherRegistrationForm.java
@@ -0,0 +1,27 @@
+package android.example.clubinformant;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TeacherRegistrationForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_registration_form);
+    }
+
+    public void initWidgets() {
+
+    }
+
+    public void signUpBtn(View view) {
+
+    }
+
+    public void goToSignInBtn(View view) {
+
+    }
+}
